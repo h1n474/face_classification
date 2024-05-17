d7a9e43f4595986cd9607aa4254178546d09a4e5
hinata@tkda <tsukadainc@gmail.com>
2024-05-17 15:03:02 +0900

    画像内に複数顔がある場合でも画像中心に一番近い人の顔を対象にする

--- main.py
+++ main.py
@@ -1,13 +1,60 @@
-from pathlib import Path
 import face_recognition
-import xattr
+import math
 import plistlib
+from PIL import Image
+from pathlib import Path
+import xattr
 
 PATH = "/img"
 
+class Faces:
+    n = 0
+    def __init__(self, img_path):
+        self.name = self.get_alphabet()
+        self.img_path = img_path
+        self.load_image = face_recognition.load_image_file(self.img_path)
+        self._encodes = self._get_encodings()
+        Faces.n += 1
+
+        if len(self._encodes) == 1:
+            self.encode = self._encodes[0]
+        else:
+            locations = self._get_locations()
+            locations_point = list(map(self._get_center_coordinates, locations))
+            locations_distance = list(map(self._get_distance_between_ImgcenterToPoint, locations_point))
+            dr_index = locations_distance.index(min(locations_distance))
+            self.encode = self._encodes[dr_index]
+
+    def _get_encodings(self)-> list:
+        return face_recognition.face_encodings(self.load_image)
+
+    def _get_locations(self)-> list:
+        return face_recognition.face_locations(self.load_image)
+
+    def _get_center_coordinates(self, box:(float, float, float, float))-> (float, float):
+        top, right, bottom, left = box
+        x = (left + right) / 2
+        y = (top + bottom) / 2
+        return (x, y)
+
+    def _get_distance_between_ImgcenterToPoint(self, point:(float,float) )-> float:
+        with Image.open(self.img_path) as img:
+            img_x = img.size[0]/2
+            img_y = img.size[1]/2
+        return math.sqrt((point[0] - img_x)**2 + (point[1] - img_y)**2)
+
+    @classmethod
+    def get_alphabet(cls):
+        #インスタンスを生成するたびに識別用のアルファベットを生成する。
+        alphabet_index = cls.n % 26  #26はアルファベットの数
+        alphabet = chr(ord('A') + alphabet_index)
+        return alphabet
+
+
+#macos_finderのラベルがあるかどうか判断する
 def has_finderTag(path):
-    # macosのfinderタグがあるか否か, タグの種類は判断してない
-    # 一度もタグ付けを指定無いファイルには'com.apple.metadata:_kMDItemUserTags'は存在しないのでtry処理。
+    #macosのfinderタグがあるか否か, タグの種類は判断してない
+    #一度もタグ付けを指定無いファイルには'com.apple.metadata:_kMDItemUserTags'は存在しないのでtry処理。
     try:
         bplist = xattr.getxattr(path, attr='com.apple.metadata:_kMDItemUserTags')
         if len(plistlib.loads(bplist)) == 0:
@@ -17,49 +64,29 @@ def has_finderTag(path):
     except OSError:
         return False
 
-class Face:
-    n = 0
-    def __init__(self, image_path):
-        self.name = self.get_alphabet()
-        self.load_image = face_recognition.load_image_file(image_path)
-        self.face_encodes = face_recognition.face_encodings(self.load_image)
-        Face.n += 1
 
-    @classmethod
-    def get_alphabet(cls):
-        # インスタンスを生成するたびに識別用のアルファベットを生成する。
-        alphabet_index = cls.n % 26  # 26はアルファベットの数
-        alphabet = chr(ord('A') + alphabet_index)
-        return alphabet
-
-if __name__ == "__main__":
-    all_imgs = list(Path(PATH).expanduser().resolve().glob('**/*.JPG'))
-    # 照合元の画像リスト
-    verification_imgs = list(filter(has_finderTag, all_imgs))
-    print(verification_imgs)
-    verification_faces = [Face(i) for i in verification_imgs]
-    verification_encode_faces = [i.face_encodes[0] for i in verification_faces]
+if __name__ == '__main__':
+    #照合するリストを作成する。
+    all_imgs = list(Path().glob('**/*.JPG'))
+    label_imgs = list(filter(has_finderTag, all_imgs))
+    src_faces = [Faces(i) for i in label_imgs]
+    src_faces_encode = [i.encode for i in src_faces]
 
     for img in all_imgs:
-        # 顔照合
-        # うまく処理がいかないファイルは後で手動でリネームするのでなにもしないことにした。
-        target_encode_face = Face(img).face_encodes
-        # 照合不可の場合代わりにNF(NotFound)を代入
-        if len(target_encode_face) == 0:
-            alphabet_name = ""
-        # 一人以上見つかった場合は　PP(Peaple)
-        elif len(target_encode_face) > 1:
-            alphabet_name = ""
-        else:
-            result = face_recognition.face_distance(verification_encode_faces, target_encode_face[0]).tolist() #numpyArrayなので最後リストに変換
+        #照合
+        try:
+            dst_encode = Faces(img).encode
+            result = face_recognition.face_distance(src_faces_encode, dst_encode).tolist() #numpyArrayなので最後リストに変換
             result_index = result.index(min(result)) #一番数値の低い数値のリストインデックスを取得
-            alphabet_name = verification_faces[result_index].name+'_'#識別アルファベット名取得
 
+            alphabet_name = src_faces[result_index].name+'_'#識別アルファベット名取得
+        except:
+            alphabet_name = ""
         print(f"{img.name} => {alphabet_name}")
 
-        # rename
+        #リネーム
         img.rename(img.parent/Path(f"{alphabet_name}{img.name}"))
-        # 同じ名前のRAWデータがあれば一緒にリネーム
+        #同じ名前のRAWデータがあれば一緒にリネーム
         raw = img.with_suffix('.CR2')
         if raw.exists():
             raw.rename(raw.parent/Path(f"{alphabet_name}{raw.name}"))
